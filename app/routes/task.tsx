b64c65192a1014d4a383a93be769002d8b3ed14a
AnonymousApplication <rgiddings20@hotmail.com>
2026-05-20 13:09:58 +0100

    Seperate forms out into components

--- app/routes/task.tsx
+++ app/routes/task.tsx
@@ -1,23 +1,11 @@
 import type { Route } from "./+types/task";
-import { Form, redirect } from 'react-router';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { redirect } from 'react-router';
 import { getFormattedDate } from "../root";
 
 import Card from 'react-bootstrap/Card';
-import FormGroup from 'react-bootstrap/FormGroup';
-import FormLabel from 'react-bootstrap/FormLabel';
-import FormControl from 'react-bootstrap/FormControl';
-import FormSelect from 'react-bootstrap/FormSelect';
-import FormText from 'react-bootstrap/FormText';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { type Task_Status } from '../types';
+import {NewTaskForm, EditDeleteTaskForm} from '../forms/task_forms'
 
 
 export function meta({ }: Route.MetaArgs) {
@@ -140,42 +128,7 @@ export default function Task({
     loaderData
 }: Route.ComponentProps) {
 
-    const { status_data, task_data, api_url } = loaderData;
-    const navigate = useNavigate();
-
-    // delete confirmation
-    const [deleteShow, setDeleteShow] = useState(false);
-    const handleNo = () => {
-        setDeleteShow(false);
-    }
-    const handleYes = () => {
-        setDeleteShow(false);
-        deleteTask();
-    }
-    const handleDeleteShow = (task_id: number) => {
-        setDeleteShow(true);
-    }
-
-    const deleteTask = async () => {
-        try {
-            const delete_response = await fetch(`${api_url}/tasks/${task_data.id}`, {
-                method: "DELETE",
-                credentials: "include",
-            });
-            if (!delete_response.ok) {
-                throw new Error(`Response status from deleting: ${delete_response.status}`);
-            }
-
-            if (delete_response.status >= 200 && delete_response.status <= 299) {
-                navigate('/');
-            }
-        }
-        catch(error) {
-            if(error instanceof Error) {
-                console.error(error.message);
-            }
-        }
-    }
+    const { status_data, task_data } = loaderData;
 
     return (
         <div>
@@ -185,116 +138,14 @@ export default function Task({
                 </Card.Header>
                 <Card.Body>
                     {task_data.id === 0 ? 
-                    <Form method="post"> 
-                        <FormGroup className="mb-3" controlId="formTaskTitle">
-                            <FormLabel>Task title</FormLabel>
-                            <FormControl 
-                                name="task_title" 
-                                aria-label="task title" 
-                                type="text" 
-                                placeholder="Enter task title"
-                                required />
-                        </FormGroup>
-                        <FormGroup className="mb-3" controlId="formTaskDescription">
-                            <FormLabel>Task description</FormLabel>
-                            <FormControl 
-                                as="textarea" 
-                                rows={3} 
-                                name="task_desc" 
-                                aria-label="task description" 
-                                placeholder="Enter task description" />
-                            <FormText className="text-muted">
-                                This is optional
-                            </FormText>
-                        </FormGroup>
-                        <FormGroup className="mb-3" controlId="formTaskStatus">
-                            <FormLabel>Task status</FormLabel>
-                            <FormSelect 
-                                name="task_status" 
-                                aria-label="task status" 
-                                defaultValue={task_data.status_id}
-                                required>
-                                {status_data.map((task_status: Task_Status) => (
-                                    <option value={task_status.id} key={task_status.id}>{task_status.status_desc}</option>
-                                ))}
-                            </FormSelect>
-                        </FormGroup>
-                        <FormGroup className="mb-3" controlId="formTaskDueDatetime">
-                            <FormLabel>When is the task due?</FormLabel>
-                            <FormControl 
-                                name="task_date" 
-                                type="date"
-                                aria-label="task date" 
-                                required />
-                            <FormControl 
-                                name="task_time" 
-                                type="time"
-                                aria-label="task time"  
-                                className="mt-2" 
-                                required />
-                        </FormGroup>
-                        <ButtonGroup aria-label="Save and Submit buttons">
-                            <Button variant="primary" type="submit">Save</Button>
-                            <Button variant="secondary" onClick={() => navigate("/", { replace: true })}>Cancel</Button>
-                        </ButtonGroup>
-                    </Form>
+                        <NewTaskForm status_data={status_data} />
                     :
                     <div>
                         <p>{task_data.task_desc ? task_data.task_desc: "No description."}</p>
                         <p><b>Due:</b> {getFormattedDate(task_data.due)}</p>
-                        <Form method="patch"> 
-                            <FormGroup as={Row} className="mb-3" controlId="formTaskTitle">
-                                <FormLabel column>Task ID</FormLabel>
-                                <Col sm="10">
-                                    <FormControl 
-                                        name="id" 
-                                        type="text"
-                                        aria-label="task id"  
-                                        defaultValue={task_data.id}
-                                        readOnly />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup as={Row} className="mb-3" controlId="formTaskStatus">
-                                <FormLabel column>Change status</FormLabel>
-                                <Col sm="10">
-                                    <FormSelect 
-                                        name="task_status" 
-                                        aria-label="task status" 
-                                        defaultValue={task_data.status_id}
-                                        required>
-                                        {status_data.map((task_status: Task_Status) => (
-                                            <option value={task_status.id} key={task_status.id}>{task_status.status_desc}</option>
-                                        ))}
-                                    </FormSelect>
-                                </Col>
-                            </FormGroup>
-                            <ButtonGroup aria-label="Save, Delete and Cancel buttons">
-                                <Button variant="primary" type="submit">Save</Button>
-                                <Button className="ms-auto" variant="danger" onClick={() => handleDeleteShow(task_data.id)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                                    </svg>
-                                </Button>
-                                <Button variant="secondary" onClick={() => navigate("/", { replace: true })}>Cancel</Button>
-                            </ButtonGroup>
-                        </Form> 
+                        <EditDeleteTaskForm task_data={task_data} status_data={status_data} />
                     </div>
                     }
-                    <Modal show={deleteShow} onHide={handleNo}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Please confirm</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>Are you sure you want to delete this task?</Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={handleNo}>
-                                No
-                            </Button>
-                            <Button variant="danger" onClick={handleYes}>
-                                Yes
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
                 </Card.Body>
                 <Card.Footer>{actionData ? actionData : ""}</Card.Footer>
             </Card>
